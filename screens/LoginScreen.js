dc4dd723dc31e3f2fe2dce2531882153f8102e87
Sufen03 <erh23976@hotmail.com>
2022-06-15 12:48:26 +0800

    login screen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,11 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
 export default function LoginScreen({ navigation }) {
   return (
     <View>
-      <Text>LoginScreen</Text>
+      <Text>Email</Text>
+      <TextInput placeholder="Enter email" />
+      <Text>Password</Text>
+      <TextInput placeholder="Enter password" secureTextEntry={true} />
     </View>
   )
 }
